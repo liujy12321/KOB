043e5a6cbb931c67538458085c5624b39da14d6f
Jiayang Liu <liujiayang12321@gmail.com>
2024-03-08 00:56:09 -0800

    implement remove and update services, controllers and test APIs

--- web/src/views/User/Bots/UserBotsIndexView.vue
+++ web/src/views/User/Bots/UserBotsIndexView.vue
@@ -15,13 +15,50 @@ export default {
     },
     setup() {
         const store = useStore();
+        // $.ajax ({
+        //     url: "http://localhost:3000/user/bot/add/",
+        //     type: "POST",
+        //     data: {
+        //         title: "the title of the bot",
+        //         description: "the description of the bot",
+        //         content: "the content of the bot",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax ({
+        //     url: "http://localhost:3000/user/bot/remove/",
+        //     type: "POST",
+        //     data: {
+        //         bot_id: 3,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
         $.ajax ({
-            url: "http://localhost:3000/user/bot/add/",
+            url: "http://localhost:3000/user/bot/update/",
             type: "POST",
             data: {
-                title: "the title of the bot",
-                description: "the description of the bot",
-                content: "the content of the bot",
+                bot_id: 4,
+                title: "bot number 5",
+                description: "the description of bot No.5",
+                content: "the content of bot No.5",
             },
             headers: {
                 Authorization: "Bearer " + store.state.user.token,
